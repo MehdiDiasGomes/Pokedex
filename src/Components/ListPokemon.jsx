21fec7bfb921801c80d9aac62fe693fb0e65b7ae
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-03-09 23:02:34 +0100

    🐛 Fix : Modification de la methode filter (suppression de filteredPokemons)

--- src/Components/ListPokemon.jsx
+++ src/Components/ListPokemon.jsx
@@ -3,7 +3,6 @@ import Card from './Card'
 import axios from 'axios'
 
 const ListPokemon = ({ start, end, searchTerm }) => {
-
   const [pokemons, setPokemons] = useState([])
 
   const URL = import.meta.env.VITE_URL_API + `pokemon?limit=${end - start}&offset=${start}`
@@ -11,7 +10,12 @@ const ListPokemon = ({ start, end, searchTerm }) => {
   const fetchData = async () => {
     try {
       const response = await axios.get(URL)
-      setPokemons(response.data.results)
+      if (response.data) {
+        if (searchTerm)
+          return setPokemons(response.data.results.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())))
+
+        setPokemons(response.data.results)
+      }
     } catch (error) {
       console.error(error)
     }
@@ -19,20 +23,18 @@ const ListPokemon = ({ start, end, searchTerm }) => {
 
   useEffect(() => {
     fetchData()
-  }, [start, end])
+  }, [start, end, searchTerm])
 
-  // const filteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
-
-  // if (pokemons && filteredPokemons.length) return <p className="absolute bottom-10 right-10 text-3xl text-white">Aucun résultat trouvé.</p>
+  if (!pokemons) return <p className="absolute bottom-10 right-10 text-3xl text-white">Aucun résultat trouvé.</p>
 
   return (
     <div className="w-full h-full flex flex-col gap-5 justify-center items-center">
       <ul className="flex justify-center items-center gap-5 p-10 bg-bodyColor lg:w-[65%] sm:w-[85%] flex-wrap rounded-lg">
         {pokemons.map((pokemon, index) => (
-          <li key={index}>
-            <Card name={pokemon.name} url={pokemon.url} />
-          </li>
-        ))}
+            <li key={index}>
+              <Card name={pokemon.name} url={pokemon.url} />
+            </li>
+          ))}
       </ul>
     </div>
   )
