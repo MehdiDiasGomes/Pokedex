3b1b50f4d7a1bbca259f99fb24faa3d885b2965b
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-03-09 22:39:09 +0100

    🐛 Fix : Fix filter with condition if (pokemons)

--- src/Components/ListPokemon.jsx
+++ src/Components/ListPokemon.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 import axios from 'axios'
 
 const ListPokemon = ({ start, end, searchTerm }) => {
-  
+
   const [pokemons, setPokemons] = useState([])
 
   const URL = import.meta.env.VITE_URL_API + `pokemon?limit=${end - start}&offset=${start}`
@@ -23,13 +23,12 @@ const ListPokemon = ({ start, end, searchTerm }) => {
 
   const filteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
-  if (filteredPokemons.length === 0) return <p className="absolute bottom-10 right-10 text-3xl text-white">Aucun résultat trouvé.</p>
-  console.log(pokemons)
+  if (pokemons && filteredPokemons.length === 0) return <p className="absolute bottom-10 right-10 text-3xl text-white">Aucun résultat trouvé.</p>
 
   return (
     <div className="w-full h-full flex flex-col gap-5 justify-center items-center">
       <ul className="flex justify-center items-center gap-5 p-10 bg-bodyColor lg:w-[65%] sm:w-[85%] flex-wrap rounded-lg">
-        {filteredPokemons.map((pokemon, index) => (
+        {pokemons && filteredPokemons.map((pokemon, index) => (
           <li key={index}>
             <Card name={pokemon.name} url={pokemon.url} />
           </li>
